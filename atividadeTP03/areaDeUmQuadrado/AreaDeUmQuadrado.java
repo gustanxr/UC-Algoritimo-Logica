41e5023a5591a6c5503921c6712de1903fcba406
gustanxr <guga_fiorillo2019@outlook.com>
2026-09-15 10:01:53 -0300

    att areadeumquadrado java

--- atividadeTP03/areaDeUmQuadrado/AreaDeUmQuadrado.java
+++ atividadeTP03/areaDeUmQuadrado/AreaDeUmQuadrado.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class AreaDeUmQuadrado{
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-
         
+        System.out.print("Qual tamanho do lado do quadrado: ");
+        double lado = sc.nextInt();
+
+        double result = Math.pow(lado, 2);
+
+        System.out.println("A = " + result);
 
     }
 }
