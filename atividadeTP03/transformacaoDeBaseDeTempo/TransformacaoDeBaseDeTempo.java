41d070e7b90294e0590b5fad6d4ab0dd0df4f00c
gustanxr <guga_fiorillo2019@outlook.com>
2026-09-25 20:13:26 -0300

    att

--- atividadeTP03/transformacaoDeBaseDeTempo/TransformacaoDeBaseDeTempo.java
+++ atividadeTP03/transformacaoDeBaseDeTempo/TransformacaoDeBaseDeTempo.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class TransformacaoDeBaseDeTempo{
     public static void main(String[]args){
         
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira o tempo em segundos");
+        int tempo = sc.nextInt();
+
+        int horas = tempo / 3600;
+        int restoHoras = tempo % 3600;
+
+        int minutos = restoHoras / 60;
+
+        int segundos = restoHoras % 60;
+
+        System.out.printf("%d hora, %d minutos e %d segundos" , horas, minutos, segundos);
     }
 }
